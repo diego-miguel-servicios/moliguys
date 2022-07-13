775f1023087f97cb64c671b21189241a84bb2c21
miguel-molinari <miguel.molinari@gmail.com>
2022-07-13 20:28:28 -0300

    Jump Character https://www.youtube.com/watch?v=ynh7b-AUSPE

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 0f;
     [SerializeField] float rotationSpeed = 0f;
 
+    [SerializeField] float jumpSpeed = 0f;
+
     private CharacterController characterController = null;
-    
+    private float ySpeed;
+    private float originalStepOffSet;
+
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        originalStepOffSet = characterController.stepOffset;
     }
 
     // Update is called once per frame
@@ -21,15 +26,33 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float magnitude = Mathf.Clamp01( movementDirection.magnitude) * speed;
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
         movementDirection.Normalize();
 
-        characterController.SimpleMove(movementDirection * magnitude);
+        ySpeed += Physics.gravity.y * Time.deltaTime;
 
-        if (movementDirection != Vector3.zero){
-            Quaternion toRotation = Quaternion.LookRotation( movementDirection, Vector3.up);
+        if (characterController.isGrounded)
+        {
+            characterController.stepOffset = originalStepOffSet;
+            ySpeed = -0.5f;
+            if (Input.GetButtonDown("Jump"))
+            {
 
-            transform.rotation =  Quaternion.RotateTowards( transform.rotation, toRotation, rotationSpeed * Time.deltaTime );
+                ySpeed = jumpSpeed;
+            }
+        } else{
+            characterController.stepOffset = 0;
+        }
+
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
